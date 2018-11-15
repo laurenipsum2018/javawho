7b42291fe650c6ba94b5d050c9647263d5a97563
laurenipsum2018 <hello@inesnorman.com>
2018-11-16 10:44:11 +1100

    i made a shape

--- canvas.js
+++ canvas.js
@@ -1,2 +1,9 @@
 var canvas = document.querySelector('canvas')
   ;
+canvas.width = window.innerWidth; // setting full width / height
+canvas.height = window.innerHeight;
+
+var c = canvas.getContext('2d'); // "magic paintbrush"
+
+c.fillRect(100, 100, 100, 100); // x and y (SHAPES: coordinates from top lhs of screen: x, y, width, height)
+console.log(canvas);
