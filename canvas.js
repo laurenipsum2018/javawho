6917900014bb4502748a715164365c7f55b0ef87
laurenipsum2018 <hello@inesnorman.com>
2018-11-16 10:50:51 +1100

    learning to draw lines

--- canvas.js
+++ canvas.js
@@ -5,5 +5,11 @@ canvas.height = window.innerHeight;
 
 var c = canvas.getContext('2d'); // "magic paintbrush"
 
-c.fillRect(100, 100, 100, 100); // x and y (SHAPES: coordinates from top lhs of screen: x, y, width, height)
+c.fillRect(100, 100, 100, 100); // Rectangle Shape: coordinates from top lhs of screen: (x, y, width, height)
 console.log(canvas);
+
+// Line
+c.beginPath();
+c.moveTo(200, 400); // (x, y) line start
+c.lineTo(400, 100); // (x, y) line end
+c.stroke();
