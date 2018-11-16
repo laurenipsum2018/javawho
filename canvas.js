ef0f2bf086f30527ab20b01c76aca8411572652e
laurenipsum2018 <hello@inesnorman.com>
2018-11-16 13:43:32 +1100

    animation

--- canvas.js
+++ canvas.js
@@ -20,20 +20,39 @@ c.lineTo(800, 400); // line 2
 c.strokeStyle = "#FA00A0"; // color
 c.stroke();
 
-// Animated square
-
-var distance = 1; // In 1 second, i'mma move 10px
+// Animated square - dealing with X coordinates
+var distance = 3; // In 1 second, i'mma move 10px
 var squarexpos = 0; // x position of square
-function draw() {
+function moverhs() {
   c.clearRect(squarexpos-distance, 700, 100, 100) // "eraser tool"
   c.fillStyle = 'rgba(0, 255, 0, 0.8)';
   c.fillRect(squarexpos, 700, 100, 100);
   if(squarexpos > canvas.width){ // if the square is off the canvas
-    squarexpos = 0 // put it back at the start
+    squarexpos = -100 // hiding to the LHS of the screen; at 0 it's the start of X
   }
   else{
-    squarexpos = squarexpos+distance // defining the movement
+    squarexpos = squarexpos+distance
   }
-  window.requestAnimationFrame(draw) // "i'mma keep drawing"
+  window.requestAnimationFrame(moverhs) // "i'mma keep drawing"
 }
-draw()
+moverhs()
+
+
+// Animated bouncy square - dealing with Y coordinates
+var speed = 4; // how fast i'm going
+var verticalspeed = speed;
+var squareypos = 0;
+function bounce() {
+  c.clearRect(400, squareypos-verticalspeed, 100, 100) // "eraser tool"
+  c.fillStyle = 'rgba(0, 100, 100, 0.8)';
+  c.fillRect(400, squareypos, 100, 100); // editing the y coordinate
+  if(squareypos > canvas.height-100){ // canvas minus the shape height
+    verticalspeed = speed*-1
+  }
+  if(squareypos < 0){
+    verticalspeed = speed
+  }
+  squareypos = squareypos+verticalspeed
+  window.requestAnimationFrame(bounce)
+}
+bounce()
